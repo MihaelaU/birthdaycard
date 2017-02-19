13d10a3eadf6304c70303a3333046cfcd540732e
MIHAELA\ursum <ursumihaela2009@gmail.com>
2017-02-19 15:21:22 +0100

    import library

--- birthdayCard/app/src/main/java/geburtstagskarten/android/com/birthdaycard/MainActivity.java
+++ birthdayCard/app/src/main/java/geburtstagskarten/android/com/birthdaycard/MainActivity.java
@@ -3,6 +3,8 @@ package geburtstagskarten.android.com.birthdaycard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import geburtstagskarten.android.com.geburtstagskarten.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
