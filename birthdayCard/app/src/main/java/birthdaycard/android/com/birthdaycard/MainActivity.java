24c52a64d99c1a672cb70ccaa3a8e709961f54c6
MIHAELA\ursum <ursumihaela2009@gmail.com>
2017-02-19 12:33:28 +0100

    new project

---
+++ birthdayCard/app/src/main/java/birthdaycard/android/com/birthdaycard/MainActivity.java
@@ -0,0 +1,13 @@
+package birthdaycard.android.com.birthdaycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
